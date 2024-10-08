130defde8aba971ff2e84a7a1610b2aff5285568
Lamz16 <andiku0755@gmail.com>
2024-10-08 20:57:12 +0700

    styling custom widget say_hi_txt.dart

--- lib/custom_widget/say_hi_txt.dart
+++ lib/custom_widget/say_hi_txt.dart
@@ -7,13 +7,27 @@ class SayHiTxt extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const colorBg = Color(0xFF66E381);
     return Container(
+      decoration: BoxDecoration(
+        color: colorBg,
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.5),
+            offset: Offset(10, 5),
+            blurRadius: 10,
+            spreadRadius: -4
+          )
+        ],
+      ),
       margin: const EdgeInsets.only(bottom: 60),
+      padding: const EdgeInsets.all(30.0),
       child: Column(
         children: [
           Text(
             text,
-            style: TextStyle(fontSize: 18.0),
+            style: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
           )
         ],
       ),
